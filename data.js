d7c8e5a6b2ed3a8009e8cfd84c929490bd587bc0
HyeHyun <gogus8281@gmail.com>
2026-08-14 18:14:33 +0900

    Update data.js

--- data.js
+++ data.js
@@ -5,7 +5,6 @@ const publicationData = [
         title: "Preventing Excessive Lottery Behaviors by Correcting Cognitive Distortions: An Online Message Experiment Study",
         authors: "Paek, H.-J., Ma, H. H., & Hove, T.",
         venue: "The Social Science Journal (In press)",
-        link: null,
         tags: ["PCC"],
         award: null
     },
